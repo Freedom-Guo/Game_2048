682997f10b918cc80180791a859c57f100828ae7
hustgtw <2354323727@qq.com>
2018-07-31 23:02:02 +1000

    Create rank.h

---
+++ rank.h
@@ -0,0 +1,26 @@
+#ifndef RANK_H
+#define RANK_H
+
+#include <QDialog>
+#include "lianbiao.h"
+
+namespace Ui {
+class Rank;
+}
+
+class Rank : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Rank(QWidget *parent = 0);
+    ~Rank();
+
+private slots:
+    void on_pushButton_clicked();
+
+private:
+    Ui::Rank *ui;
+};
+
+#endif // RANK_H
